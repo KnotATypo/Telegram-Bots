40962e2f6faf5f9e15d313f6886ea76c8245e085
Josh <joshua.mackay17@gmail.com>
2025-11-03 21:11:19 +1000

    Added time estimator tool

--- src/telegram_bots/tools/tools_bot.py
+++ src/telegram_bots/tools/tools_bot.py
@@ -1,12 +1,14 @@
 import json
 import os
+from datetime import datetime
+from typing import Tuple
 
 import requests
 
 from telegram_bots.bot import Bot
 from telegram_bots.tools.util import get_power_draw
 
-STATES = ["Power meter"]
+STATES = ["Power meter", "Check estimate"]
 CUSTOM_KEYBOARD = {
     "keyboard": [[{"text": "Power meter"}]],
     "resize_keyboard": True,
@@ -17,6 +19,7 @@ CUSTOM_KEYBOARD = {
 
 class ToolsBot(Bot):
     state: str
+    time_estimate: dict[str, Tuple[datetime, int]] = {}
 
     def __init__(self):
         print("Initialising ToolsBot...")
@@ -28,15 +31,27 @@ class ToolsBot(Bot):
         message = data["message"]
         chat_id = message["chat"]["id"]
 
-        if self.state == "idle":
+        if "text" in message and message["text"].lower() == "done":
+            start_time, estimate = self.time_estimate[chat_id]
+            actual = (datetime.now() - start_time).seconds / 60
+            self.send_message(
+                f"Estimate: {estimate}\nActual: {actual}\nDifference: {100 / estimate * actual}%",
+                chat_id,
+                replay_markup=CUSTOM_KEYBOARD,
+            )
+        elif self.state == "idle":
             if message["text"] in STATES:
                 self.state = message["text"]
                 if self.state == "Power meter":
                     self.send_message("Please send video", chat_id, replay_markup={"remove_keyboard": True})
+                elif self.state == "Check estimate":
+                    self.send_message("Provide estimate in minutes", chat_id)
             else:
-                self.send_message("What?", chat_id, replay_markup=CUSTOM_KEYBOARD)
+                self.send_message("Please selection from options", chat_id, replay_markup=CUSTOM_KEYBOARD)
         elif self.state == "Power meter":
             self.read_power_meter(chat_id, message)
+        elif self.state == "Check estimate":
+            self.store_estimate(chat_id, message["text"])
 
     def read_power_meter(self, chat_id, message):
         if "video" in message:
@@ -60,4 +75,13 @@ class ToolsBot(Bot):
         else:
             self.send_message("That wasn't a video, try again", chat_id)
 
-
+    def store_estimate(self, chat_id: str, message: str):
+        try:
+            estimate_minutes = int(message)
+            self.time_estimate[chat_id] = (datetime.now(), estimate_minutes)
+            self.send_message(
+                'Estimate stored. Send "done" to complete estimate', chat_id, replay_markup=CUSTOM_KEYBOARD
+            )
+            self.state = "idle"
+        except ValueError:
+            self.send_message("Please provide an estimate in minutes", chat_id)
